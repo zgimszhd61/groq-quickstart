fd7da2a16e8164bd95d8f3af8bd3254f8558a013
xiaoming <lininruc@gmail.com>
2024-03-29 22:16:31 +0800

    Add mvs

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key="",
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of low latency LLMs",
+        }
+    ],
+    model="mixtral-8x7b-32768",
+)
+
+print(chat_completion.choices[0].message.content)
